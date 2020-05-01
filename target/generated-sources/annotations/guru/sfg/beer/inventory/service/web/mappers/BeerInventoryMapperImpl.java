5501b107284f6d9bc6d13c9ed97978da1ffe8217
mwunderlich <martin@wunderlich.com>
2020-05-01 20:11:39 +0200

    Add propertties for JMS

--- target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
+++ target/generated-sources/annotations/guru/sfg/beer/inventory/service/web/mappers/BeerInventoryMapperImpl.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2020-04-19T15:37:47+0200",
+    date = "2020-05-01T20:04:40+0200",
     comments = "version: 1.3.0.Final, compiler: javac, environment: Java 11.0.1 (Oracle Corporation)"
 )
 @Component
